731da0cb0d7db12dcfd7f41a378a46778ce5dd31
Ahejia <951179195@qq.com>
2019-08-22 09:55:49 +0800

    SpringBoot打成的war包添加扫描的注解

--- src/main/java/com/example/xnlogin/XnloginApplication.java
+++ src/main/java/com/example/xnlogin/XnloginApplication.java
@@ -3,10 +3,18 @@ package com.example.xnlogin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+@ServletComponentScan
 @SpringBootApplication
 @MapperScan("com.example.xnlogin.mapper")
-public class XnloginApplication {
+public class XnloginApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XnloginApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(XnloginApplication.class, args);
