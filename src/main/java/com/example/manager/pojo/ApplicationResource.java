d2f14ed651349b7bec08370f65daa456e3255dfc
Ahejia <951179195@qq.com>
2019-10-31 09:41:03 +0800

    添加类的说明

--- src/main/java/com/example/manager/pojo/ApplicationResource.java
+++ src/main/java/com/example/manager/pojo/ApplicationResource.java
@@ -1,52 +1,65 @@
 package com.example.manager.pojo;
 
 import com.example.manager.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * @Author hj
  * @Description 应用资源表
  * @Date 16:31 2019/10/22
-**/
+ **/
+@ApiModel("应用资源")
 public class ApplicationResource extends BaseEntity {
     /**
      * 资源编码
      */
+    @ApiModelProperty("资源编号（例如00001）")
     private String code;
     /**
      * 资源名称
      */
+    @ApiModelProperty("资源名称")
     private String name;
     /**
      * 资源类型
      */
+    @ApiModelProperty("资源类型")
     private String type;
     /**
      * 资源url
      */
+    @ApiModelProperty("资源url")
     private String url;
     /**
      * 是否被禁用（0启用1禁用）
      */
+    @ApiModelProperty("状态(0启用1禁用)")
     private Short status;
     /**
      * 备注
      */
+    @ApiModelProperty("备注")
     private String remark;
     /**
      * 父id
      */
+    @ApiModelProperty("父id")
     private Long parentId;
     /**
      * 排序
      */
+    @ApiModelProperty("排序")
     private Double sort;
     /**
      * 是否开启授权(0未开启1开启授权)
      */
+    @ApiModelProperty("是否开启授权（0未开启1开启授权）")
     private Short authorization;
     /**
      * 应用id
      */
+    @ApiModelProperty("应用编号")
     private Long applicationId;
 
     public String getCode() {
--- src/main/java/com/example/manager/pojo/ApplicationRole.java
+++ src/main/java/com/example/manager/pojo/ApplicationRole.java
@@ -1,20 +1,35 @@
 package com.example.manager.pojo;
 
 import com.example.manager.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * @Author hj
  * @Description  应用角色表
  * @Date 16:31 2019/10/22
-**/
+ **/
+@ApiModel("应用角色")
 public class ApplicationRole extends BaseEntity {
-    /**应用id*/
+    /**
+     * 应用id
+     */
+    @ApiModelProperty("应用编号")
     private Long applicationId;
-    /**名称*/
+    /**
+     * 名称
+     */
+    @ApiModelProperty("角色名称")
     private String name;
-    /**备注*/
+    /**
+     * 备注
+     */
+    @ApiModelProperty("备注")
     private String remark;
-    /**0启用1禁用*/
+    /**
+     * 0启用1禁用
+     */
+    @ApiModelProperty("状态(0启用1禁用)")
     private Short status;
 
     public Long getApplicationId() {
